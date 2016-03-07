751e8b5e78f10530f243b58f0a7c30a978b7808a
dinesh khadka <dnshka@gmail.com>
2016-03-07 10:30:59 +0545

    added junkiree and vesak

---
+++ junkiree/js/main.js
@@ -0,0 +1,54 @@
+
+
+
+
+var mainPage = document.getElementById("main-page");
+var featuresPage = document.getElementById("features-page");
+var aboutPage = document.getElementById("about-page");
+
+var homeLink = document.getElementById("home-link");
+var featuresLink = document.getElementById("features-link");
+var aboutLink = document.getElementById("about-link");
+
+function clickity(id){
+
+	if (id == 0){
+		mainPage.classList.add('hide')
+		featuresPage.classList.remove('hide')
+		aboutPage.classList.add('hide')
+	}
+
+	else if (id == 1){
+		featuresPage.classList.add('hide')
+		mainPage.classList.remove('hide')
+		aboutPage.classList.add('hide')
+	}
+	else if (id == 2){
+		featuresPage.classList.add('hide')
+		mainPage.classList.add('hide')
+		aboutPage.classList.remove('hide')
+	}
+}
+
+function snoop(){
+
+	var getCurrentHash = window.location['hash'];
+	if (getCurrentHash == "#main"){
+		clickity(1)
+	}
+	else if(getCurrentHash == "#features"){
+		clickity(0)
+	}
+	else if(getCurrentHash == "#about"){
+		clickity(2)
+	}
+	else{
+		clickity(1)
+	}
+}
+
+
+window.addEventListener('hashchange', snoop, false)
+window.onload = snoop
+
+
---
+++ vesak/Readme.md
@@ -0,0 +1,14 @@
+# vesak.js
+##### What is Vesak.js?
+
+Vesak.js is just another date converter that does nothing more than any other date converters. I put the `.js` to make it sound cool.
+
+##### Why does it exist then?
+It's a simple experiment with a client side date conversion to add something to my portfolio. Almost all converters seem to utilize the backend for this causing a slow conversion.
+
+
+##### Thanks
+I would like to thank the amazing [Prashish Rajbhandari](http://prashishrajbhandari.com.np/) for his extremely useful [libraries](https://github.com/prashishh)
+ 
+##### Credits?
+Vesak is built using SASS, jQuery, the awesome Sublime 3 text editor powered by tea.
